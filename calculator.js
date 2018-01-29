49c18f813b758c39a5613a422b1ecac313697e17
martyanovaa <amartyanova98@gmail.com>
2018-01-29 12:20:51 +0200

    change doc

---
+++ calculator.js
@@ -0,0 +1,13 @@
+var FirstNumber;
+var SecondNumber;
+
+FirstNumber = Number(prompt("input First Number"))
+SecondNumber = Number(prompt("input Firt Number"))
+var sum = FirstNumber + SecondNumber;
+var sum2 = FirstNumber - SecondNumber;
+var sum3 = FirstNumber / SecondNumber;
+var sum4 = FirstNumber * SecondNumber;
+document.write(sum + "<br>");
+document.write(sum2 + "<br>");
+document.write(sum3 + "<br>");
+document.write(sum4);
